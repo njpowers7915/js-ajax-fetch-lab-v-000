72d737975e87cadaafd5cc0adab30c48ab2b33b8
njpowers7915 <njpowers7915@gmail.com>
2018-08-12 12:13:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
+
 function getIssues() {
 }
 
@@ -13,6 +19,11 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch('https://github.com/learn-co-curriculum/javascript-fetch-lab', {
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json()).then(json => console.log(json));
 }
 
 function getToken() {
@@ -20,3 +31,5 @@ function getToken() {
   //back to '' before committing so all tests pass
   return ''
 }
+
+TEST
